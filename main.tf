6f580076eb1bc06e5436c58f65f2c66b76ab8049
Paulo Junior <phjph@hotmail.com>
2023-09-13 00:10:18 -0300

    Initial commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+ami = "ami-04cb4ca688797756f"
+instance_type = "t2.micro"
+
+tags = {
+    Name = "terraform-example"
+}
+}
